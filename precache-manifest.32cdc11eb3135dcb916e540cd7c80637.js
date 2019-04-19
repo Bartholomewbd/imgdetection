da816554c16ff0193a1adfe032d2ab27899d0730
Brian Bartholomew <bartholomewbd@gmail.com>
2019-04-19 15:46:06 -0400

    Updates

---
+++ precache-manifest.32cdc11eb3135dcb916e540cd7c80637.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c65854001a1236ce58a",
+    "url": "/imgdetection/static/css/main.1c8de250.chunk.css"
+  },
+  {
+    "revision": "2c65854001a1236ce58a",
+    "url": "/imgdetection/static/js/main.252bf985.chunk.js"
+  },
+  {
+    "revision": "dc12cdc5309dc0ef6ced",
+    "url": "/imgdetection/static/js/runtime~main.8aec0afc.js"
+  },
+  {
+    "revision": "9c56c81c058a9033cceb",
+    "url": "/imgdetection/static/css/2.17f7f4ab.chunk.css"
+  },
+  {
+    "revision": "9c56c81c058a9033cceb",
+    "url": "/imgdetection/static/js/2.33497142.chunk.js"
+  },
+  {
+    "revision": "24088577d56eee3a5f6693f7a83f49a9",
+    "url": "/imgdetection/static/media/camera.24088577.png"
+  },
+  {
+    "revision": "faf5712435784a6c971f4e256a09f359",
+    "url": "/imgdetection/index.html"
+  }
+];
